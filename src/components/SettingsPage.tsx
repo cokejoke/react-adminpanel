e3b323ff4d3394639bbbdf0049c21ee073b37e62
micmine <micmine4@gmail.com>
2020-04-25 21:38:30 +0200

    :lipstick: Add Settings Expansion Panel & SettingsStore

--- src/components/SettingsPage.tsx
+++ src/components/SettingsPage.tsx
@@ -1,41 +1,142 @@
 import React from "react";
 import Navigation from "./Navigation";
-import { WithStyles, Theme, withStyles, createStyles, Typography } from "@material-ui/core";
+import {
+  WithStyles,
+  Theme,
+  withStyles,
+  createStyles,
+  Typography,
+} from "@material-ui/core";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
 import { StoreHolder } from "../store/StoreHolder";
+import SettingsStore from "../store/SettingsStore";
+import { observer } from "mobx-react";
 
 const styles = (theme: Theme) =>
-    createStyles({
-        toolbar: {
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-            padding: theme.spacing(0, 1),
-            // necessary for content to be below app bar
-            ...theme.mixins.toolbar,
-        },
-        content: {
-            flexGrow: 1,
-            padding: theme.spacing(3),
-            margin: "0 45px"
-        }
-    });
+  createStyles({
+    toolbar: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "flex-end",
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      margin: "0 45px",
+    },
+  });
 
 type Props = WithStyles;
-
+//const [expanded, setExpanded] = React.useState<string | false>(false);
+@observer
 class SettingsPage extends React.Component<Props> {
+  private settingsStore: SettingsStore = new SettingsStore();
 
-    public render() {
-        StoreHolder.drawerStore.setName = "Settings";
-        return (
-            <div>
-                <Navigation />
-                <main className={this.props.classes.content}>
-                    <div className={this.props.classes.toolbar} />
-                    <Typography variant="h1">TEST2</Typography>
-                </main>
-            </div>
-        );
-    }
+  public render() {
+    //StoreHolder.drawerStore.setName = "Settings";
+
+    const handleChange = (panel: string) => (
+      event: React.ChangeEvent<{}>,
+      isExpanded: boolean
+    ) => {
+      this.settingsStore.expansionExpanded = isExpanded ? panel : false;
+      //setExpanded(isExpanded ? panel : false);
+    };
+    return (
+      <div>
+        <Navigation />
+        <main className={this.props.classes.content}>
+          <ExpansionPanel
+            expanded={this.settingsStore.expansionExpanded === "panel1"}
+            onChange={handleChange("panel1")}
+          >
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <Typography>General settings</Typography>
+              <Typography>I am an expansion panel</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>
+                Nulla facilisi. Phasellus sollicitudin nulla et quam mattis
+                feugiat. Aliquam eget maximus est, id dignissim quam.
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+          <ExpansionPanel
+            expanded={this.settingsStore.expansionExpanded === "panel2"}
+            onChange={handleChange("panel2")}
+          >
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2bh-content"
+              id="panel2bh-header"
+            >
+              <Typography>Users</Typography>
+              <Typography>You are currently not an owner</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>
+                Donec placerat, lectus sed mattis semper, neque lectus feugiat
+                lectus, varius pulvinar diam eros in elit. Pellentesque
+                convallis laoreet laoreet.
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+          <ExpansionPanel
+            expanded={this.settingsStore.expansionExpanded === "panel3"}
+            onChange={handleChange("panel3")}
+          >
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel3bh-content"
+              id="panel3bh-header"
+            >
+              <Typography>Advanced settings</Typography>
+              <Typography>
+                Filtering has been entirely disabled for whole web server
+              </Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>
+                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl.
+                Integer sit amet egestas eros, vitae egestas augue. Duis vel est
+                augue.
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+          <ExpansionPanel
+            expanded={this.settingsStore.expansionExpanded === "panel4"}
+            onChange={handleChange("panel4")}
+          >
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel4bh-content"
+              id="panel4bh-header"
+            >
+              <Typography>Personal data</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>
+                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl.
+                Integer sit amet egestas eros, vitae egestas augue. Duis vel est
+                augue.
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default withStyles(styles)(SettingsPage);
