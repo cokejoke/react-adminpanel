3ba6fb9cee1d0d209a4b5b949aae66b95b08b95f
micmine <micmine4@gmail.com>
2020-05-12 19:52:25 +0200

    :construction: Add ThemeTranslator

---
+++ src/helpers/ThemeTranslator.ts
@@ -0,0 +1,53 @@
+import React from "react";
+
+import red from "@material-ui/core/colors/red";
+import pink from "@material-ui/core/colors/pink";
+import purple from "@material-ui/core/colors/purple";
+import deepPurple from "@material-ui/core/colors/deepPurple";
+import indigo from "@material-ui/core/colors/indigo";
+import blue from "@material-ui/core/colors/blue";
+import lightBlue from "@material-ui/core/colors/lightBlue";
+import cyan from "@material-ui/core/colors/cyan";
+import teal from "@material-ui/core/colors/teal";
+import green from "@material-ui/core/colors/green";
+import lightGreen from "@material-ui/core/colors/lightGreen";
+import lime from "@material-ui/core/colors/lime";
+import yellow from "@material-ui/core/colors/yellow";
+import amber from "@material-ui/core/colors/amber";
+import orange from "@material-ui/core/colors/orange";
+import deepOrange from "@material-ui/core/colors/deepOrange";
+import brown from "@material-ui/core/colors/brown";
+import grey from "@material-ui/core/colors/grey";
+import blueGrey from "@material-ui/core/colors/blueGrey";
+import { Theme } from "@material-ui/core";
+
+class ThemeTranslator {
+  static toString = (color: Theme) => {};
+
+  static toTheme = (color: string) => {
+    if (color === "#f44336") {
+      return red;
+    } else if (color === "") {
+    } else if (color === "") {
+    } else if (color === "") {
+    } else if (color === "") {
+    } else if (color === "") {
+    } else if (color === "") {
+    } else if (color === "") {
+    } else if (color === "") {
+    } else if (color === "") {
+    } else if (color === "") {
+    } else if (color === "") {
+    } else if (color === "") {
+    } else if (color === "") {
+    } else if (color === "") {
+    } else if (color === "") {
+    } else if (color === "") {
+    } else if (color === "") {
+    } else if (color === "") {
+    } else if (color === "") {
+    }
+  };
+}
+
+export default ThemeTranslator;
